69bf1905fb9886ff71910874b999f3f7819bd2cf
Amith <amithkumar9906@gmail.com>
2026-03-01 10:29:23 +0530

    setting ipv4

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
+const dns = require("dns");
 
+dns.setDefaultResultOrder("ipv4first");
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   logging: process.env.NODE_ENV === 'development' ? console.log : false,
